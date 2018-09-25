88b30d0f7198d7676ff19824cf482370b9b4a3cb
kalpawijesooriya <kalpa.wijesooriya@yahoo.com>
2018-09-25 21:32:10 +0530

    Stsrt Issue managementSystem
    Stsrt Issue managementSystem

---
+++ IssueManagementSystem/IssueManagementSystem/Controllers/SupervisorController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace IssueManagementSystem.Controllers
+{
+    public class SupervisorController : Controller
+    {
+        // GET: Supervisor
+        public ActionResult selectIssue()
+        {
+            return View();
+        }
+    }
+}
