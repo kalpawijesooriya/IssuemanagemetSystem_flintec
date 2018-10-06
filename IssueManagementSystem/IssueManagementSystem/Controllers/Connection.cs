5ec421bc5b6722c2b5015e11f80d5b32ec48ebb5
Rasitha Gamage <g.rasitha@gmail.com>
2018-10-06 23:16:56 +0530

    dsd

---
+++ IssueManagementSystem/IssueManagementSystem/Controllers/Connection.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace IssueManagementSystem.Controllers
+{
+    public class Connection : Controller
+    {
+        // GET: Connection
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
