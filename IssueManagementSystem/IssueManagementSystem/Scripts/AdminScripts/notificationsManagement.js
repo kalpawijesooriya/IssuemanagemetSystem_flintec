2573837ce93b3999d5d2355556ea7c58210945dc
Rasitha Gamage <g.rasitha@gmail.com>
2018-10-12 13:08:53 +0530

    commit

---
+++ IssueManagementSystem/IssueManagementSystem/Scripts/AdminScripts/notificationsManagement.js
@@ -0,0 +1,16 @@
+﻿
+$('#selectdepartment').select2({
+    placeholder: "Select Department.....",
+    allowClear: false
+});
+
+$('#selectLine').select2({
+    placeholder: "Select Line....",
+    allowClear: false
+});
+
+
+$('#selectIssue').select2({
+    placeholder: "Select Issue....",
+    allowClear: false
+});
