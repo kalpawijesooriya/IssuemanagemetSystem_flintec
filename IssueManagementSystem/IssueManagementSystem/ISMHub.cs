161e07018e1caaf52d8bbf3ca500f4e356b099f1
kalpawijesooriya <kalpa.wijesooriya@yahoo.com>
2018-10-17 17:45:29 +0530

    add disply items

--- IssueManagementSystem/IssueManagementSystem/ISMHub.cs
+++ IssueManagementSystem/IssueManagementSystem/ISMHub.cs
@@ -2,15 +2,18 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using IssueManagementSystem.Models;
 using Microsoft.AspNet.SignalR;
 
 namespace IssueManagementSystem
 {
-	public class ISMHUB:Hub
-	{
-        public void Announce(String message)
+    public class ISMHUB : Hub
+    {
+        public void Announce()
         {
-            Clients.All.Announce(message);  
+
+            Clients.All.Announce();
+
         }
-	}
+    }
 }
