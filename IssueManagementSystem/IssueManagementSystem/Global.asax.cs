196094c26eb75aed3ddebaca6e90b472871a6ab2
kalpawijesooriya <kalpa.wijesooriya@yahoo.com>
2018-11-11 18:17:59 +0530

    update the supervisor

--- IssueManagementSystem/IssueManagementSystem/Global.asax.cs
+++ IssueManagementSystem/IssueManagementSystem/Global.asax.cs
@@ -11,8 +11,14 @@ namespace IssueManagementSystem
 {
     public class MvcApplication : System.Web.HttpApplication
     {
+      
+        
+
         protected void Application_Start()
+
+
         {
+
             AreaRegistration.RegisterAllAreas();
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
