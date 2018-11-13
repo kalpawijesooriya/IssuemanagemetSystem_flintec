417b2277bae841d3f4dec0fb3fcd053ea4708fad
kalpawijesooriya <kalpa.wijesooriya@yahoo.com>
2018-11-13 14:24:11 +0530

    few Changers

--- IssueManagementSystem/IssueManagementSystem/Controllers/SupervisorController.cs
+++ IssueManagementSystem/IssueManagementSystem/Controllers/SupervisorController.cs
@@ -138,6 +138,7 @@ namespace IssueManagementSystem.Controllers
         [HttpPost]//add Tecnical Issues to database
         public ActionResult AddIssueTechnical(issue_occurrence issueModel)
         {
+            
             var time = DateTime.Now;
             string current_time = time.ToString("yyyy-MM-dd HH:mm:ss");
 
@@ -151,7 +152,7 @@ namespace IssueManagementSystem.Controllers
                     issueModel.line_line_id = lineInfo.line_line_id;
                     issueModel.issue_satus = "1";
                     issueModel.issue_issue_ID = 3;//Issue id is 2 for Machine Brakedown
-                    issueModel.responsible_person_emp_id = 5;//get specific employee 
+                    issueModel.responsible_person_emp_id = 44;//get specific employee 
                     var date = DateTime.ParseExact(current_time, "yyyy-MM-dd HH:mm:ss", System.Globalization.CultureInfo.InvariantCulture);
                     issueModel.issue_date = date;
                     issueModel.location = (string)Session["location"];
@@ -241,8 +242,6 @@ namespace IssueManagementSystem.Controllers
                         com.setCD(cd);
                     }
               
-                
-               
             }
             });
             t.Start();
