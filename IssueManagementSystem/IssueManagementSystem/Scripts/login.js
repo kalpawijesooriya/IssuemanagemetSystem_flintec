2b0d2396b4bdaf33fb928c05b6feb69e46ed7be8
kalpawijesooriya <kalpa.wijesooriya@yahoo.com>
2018-12-13 17:16:22 +0530

    commit

--- IssueManagementSystem/IssueManagementSystem/Scripts/login.js
+++ IssueManagementSystem/IssueManagementSystem/Scripts/login.js
@@ -1,4 +1,5 @@
-﻿$.getScript("https://cdnjs.cloudflare.com/ajax/libs/particles.js/2.0.0/particles.min.js", function () {
+﻿/// <reference path="particles.min.js" />
+$.getScript("particles.min.js", function () {
     particlesJS('particles-js',
         {
             "particles": {
