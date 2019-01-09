8ef70f0fcf5c378559a1e4627e63a46e5a9f21df
Rasitha Gamage <g.rasitha@gmail.com>
2019-01-09 18:17:51 +0530

    commit

--- IssueManagementSystem/IssueManagementSystem/Controllers/ManagerController.cs
+++ IssueManagementSystem/IssueManagementSystem/Controllers/ManagerController.cs
@@ -173,15 +173,24 @@ namespace IssueManagementSystem.Controllers
         }
 
         private class tempClass5 {
-            public DateTime issue_date { get; set; }
-            public String issue { get; set; }
+            public DateTime issue_date {get; set;}
+            public int issue_occurrence_id {get; set;}
+            public String issue {get;set;}
+            public String line_name {get; set;}
+            public String issue_satus {get; set;}
         }
 
         [HttpPost]
         public JsonResult loadIssueList() {
             using (issue_management_systemEntities1 db = new issue_management_systemEntities1())
             {
-                String query = "";
+                String query = @"SELECT issue_occurrence.issue_date,
+                                issue_occurrence.issue_occurrence_id,
+                                issues.issue,lines.line_name,issue_occurrence.issue_satus
+                                FROM issue_occurrence,lines,issues WHERE
+                                lines.line_id = issue_occurrence.line_line_id AND
+                                issues.issue_id LIKE issue_occurrence.issue_issue_ID";
+
                 var data = db.Database.SqlQuery<tempClass5>(query).ToList();
                 return Json(data, JsonRequestBehavior.AllowGet);
             }
